d1e6f9ea634b8f6e1b8881186f9017fbe74736d8
Käkäsh <marco.zillgen23@gmail.com>
2023-08-15 03:59:12 +0200

    joa war spät

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
-
+const { getSpotifyAuthURL, getSpotifyContent } = require("./spotify");
 const PORT = process.env.PORT || 3000;
 
 function startServer() {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  app.get("/", (req, res) => {
+    res.redirect(getSpotifyAuthURL());
   });
+
+  app.get("/callback", (req, res) => {
+    console.log(req.query.code);
+
+    getSpotifyContent(req.query.code).then((data) => {
+      console.log(data);
+      res.send("Hello World!");
+    });
+  });
+
+  app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 }
+
+startServer();
 
 module.exports = startServer;
--- src/spotify.js
+++ src/spotify.js
@@ -8,21 +8,68 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 const CALLBACK_URL = `http://localhost:${PORT}/callback/`;
 
-
-function connectSpotify() {
+function getSpotifyAuthURL(
+  scopes = ["user-read-playback-state", "user-read-currently-playing"]
+) {
   const spotifyApi = new SpotifyWebApi({
     clientId: CLIENT_ID,
     clientSecret: CLIENT_SECRET,
     redirectUri: CALLBACK_URL,
   });
 
-  const scopes = ["user-read-playback-state", "user-read-currently-playing"];
-
   const authorizeURL = spotifyApi.createAuthorizeURL(scopes);
 
-  
+  return authorizeURL;
+}
+
+async function getSpotifyContent(code) {
+  const spotifyApi = new SpotifyWebApi({
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    redirectUri: CALLBACK_URL,
+  });
+
+  return spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      spotifyApi.setAccessToken(data.body["access_token"]);
+      spotifyApi.setRefreshToken(data.body["refresh_token"]);
+
+      spotifyApi.getMyCurrentPlaybackState().then((data) => {
+        const songName = data.body.item.name;
+        const artistNames = data.body.item.artists.map((artist) => artist.name);
+        const albumName = data.body.item.album.name;
+        const albumCover = data.body.item.album.images[0].url;
+        const trackId = data.body.item.id;
+
+        console.log(trackId);
+
+        fetch(`https://spotify-lyric-api.herokuapp.com/?trackid=${trackId}`)
+          .then((res) => res.json())
+          .then((data) => {
+            const lyrics = data.lyric;
+            const lyricsURL = data.url;
+
+            console.log(lyrics);
+
+            const songInfo = {
+              songName,
+              artistNames,
+              albumName,
+              albumCover,
+              lyrics,
+              lyricsURL,
+            };
+
+            return songInfo;
+          })
+          .catch((err) => console.log(err));
+      });
+    })
+    .catch((err) => console.log(err));
 }
 
 module.exports = {
-  connectSpotify,
+  getSpotifyAuthURL,
+  getSpotifyContent,
 };
